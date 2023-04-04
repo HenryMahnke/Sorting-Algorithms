79e30a02acad557d97474763a154eff700ee3368
Hman16 <henry.mahnke16@gmail.com>
2023-04-03 23:17:14 -0500

    counting sort 50%

--- CountingSort.py
+++ CountingSort.py
@@ -15,12 +15,6 @@ def CountingSort(arr):
         mod_temp[n] -= 1
     print("mod_temp", mod_temp)
     print("arr", arr)
-    for i in range(len(arr)):
-        index = arr[mod_temp[i]]-1
-        print("index", index)
-        print(arr[i])
-        output[index] = arr[i]
-    print(output)
 
 arr = [1,4,1,2,7,5,2]
 CountingSort(arr)
