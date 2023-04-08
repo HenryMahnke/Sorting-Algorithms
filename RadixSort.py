db7da213567c5ca256978e8973a77700448002db
Hman16 <henry.mahnke16@gmail.com>
2023-04-07 21:40:55 -0500

    RadixSort

--- RadixSort.py
+++ RadixSort.py
@@ -1,7 +1,31 @@
-def counting_sort(A,digit,radix):
-    # this initializes an array of length n
-    Sorted = [0] * len(A)
-    C = [0]*int(radix)
+def RadixSort(arr):
+    max_value = max(arr) 
+    exp = 1
 
-    for i in range(0,len(A)):
-        digit_of_Ai = (A[i]/radix**digit)%radix
+    while exp<= max_value:
+        buckets = [[] for _ in range(10)]
+        # this makes this:
+        # [[], [], [], [], [], [], [], [], [], []]
+        # an array for each possible digit
+        print(buckets)
+        for n in arr:
+            cur_digit = (n//exp) % 10 #extracts the current digit
+            buckets[cur_digit].append(n)
+            print(buckets)
+            # this will result in something like this
+            # [[170, 90], [], [802, 2], [], [24], [45, 75], [66], [], [], []]        
+        arr=[]
+        for bucket in buckets:
+            arr.extend(bucket)
+            # concatenates the arrays back together
+        # [170, 90, 802, 2, 24, 45, 75, 66] results in this
+
+        # now because they are already sorted by the least significant bit
+        # when it loops over, any single digits, will be sorted sequentially
+        # so if you have 2, 3 in the arr
+        # those will fall into place on the next pass
+        exp *= 10
+    return arr
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+arr = RadixSort(arr)
+print(arr)
